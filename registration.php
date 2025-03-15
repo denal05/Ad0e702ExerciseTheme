dae578e500e4e0213d08028226bd70620e4da595
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2025-03-15 17:53:27 +0100

    Added bare minimum files for a demo theme: registration.php, theme.xml, and media/<preview-image>

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Denal05/Ad0e702ExerciseTheme',
+    __DIR__
+);
